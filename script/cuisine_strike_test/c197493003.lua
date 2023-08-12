f266d69dfb91186e58f5c09a31f20c43ee4fe484
WSKKung <armkung493@gmail.com>
2023-08-12 10:20:58 +0700

    Remove def condition & change def gain

--- script/cuisine_strike_test/c197493003.lua
+++ script/cuisine_strike_test/c197493003.lua
@@ -7,7 +7,7 @@ local s, id = GetID()
 Duel.LoadScript("cuisine_strike_common.lua")
 
 s.extra_def_min_def = 500
-s.extra_def_multiplier = 100
+s.extra_def_multiplier = 200
 
 function s.initial_effect(c)
 	CS.InitCommonEffects(c)
@@ -22,7 +22,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCondition(s.condition)
+	--e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
@@ -38,6 +38,12 @@ end
 --- @type CostFunction
 function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk==0 then return Duel.IsPlayerCanDiscardDeckAsCost(tp, 1) end
+	local tc = Duel.GetDecktopGroup(tp, 1):GetFirst()
+	if CS.IsIngredientCard(tc) then
+		e:SetLabel(CS.GetGrade(tc))
+	else
+		e:SetLabel(0)
+	end
 	Duel.DiscardDeck(tp, 1, REASON_COST)
 end
 
@@ -49,13 +55,16 @@ end
 --- @type OperationFunction
 function s.operation(e, tp, eg, ep, ev, re, r, rp)
 	local c = e:GetHandler()
-	local extra_def_e = Effect.CreateEffect(c)
-	extra_def_e:SetType(EFFECT_TYPE_SINGLE)
-	extra_def_e:SetCode(EFFECT_UPDATE_DEFENSE)
-	extra_def_e:SetRange(LOCATION_MZONE)
-	extra_def_e:SetValue(function (e)
-		return CS.GetGrade(e:GetHandler()) * s.extra_def_multiplier
-	end)
-	extra_def_e:SetReset(RESET_EVENT + RESETS_STANDARD_DISABLE)
-	c:RegisterEffect(extra_def_e)
+	local cost_grade = e:GetLabel()
+	if cost_grade > 0 then
+		local extra_def_e = Effect.CreateEffect(c)
+		extra_def_e:SetType(EFFECT_TYPE_SINGLE)
+		extra_def_e:SetCode(EFFECT_UPDATE_DEFENSE)
+		extra_def_e:SetRange(LOCATION_MZONE)
+		extra_def_e:SetValue(function (e)
+			return cost_grade * s.extra_def_multiplier
+		end)
+		extra_def_e:SetReset(RESET_EVENT + RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(extra_def_e)
+	end
 end
