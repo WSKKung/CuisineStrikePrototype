a70a8d0374f9285c77aeec4b80211b13826a8cd0
WSKKung <armkung493@gmail.com>
2023-08-13 13:51:09 +0700

    Add card effect

--- script/cuisine_strike_test/c197493019.lua
+++ script/cuisine_strike_test/c197493019.lua
@@ -14,4 +14,25 @@ function s.initial_effect(c)
 	-- cook summon procedures
 	Fusion.AddProcMixRep(c, true, true, aux.FilterBoolFunctionEx(Card.IsRace, CS.CLASS_MILK), 1, 2)
 
+	local debuff_e = Effect.CreateEffect(c)
+	debuff_e:SetType(EFFECT_TYPE_FIELD)
+	debuff_e:SetCode(EFFECT_UPDATE_ATTACK)
+	debuff_e:SetRange(LOCATION_MZONE)
+	debuff_e:SetTargetRange(0, LOCATION_MZONE)
+	debuff_e:SetTarget(aux.TRUE)
+	debuff_e:SetValue(s.value)
+	c:RegisterEffect(debuff_e)
+
+	local ingredient_debuff_e = debuff_e:Clone()
+	ingredient_debuff_e:SetRange(LOCATION_SZONE)
+	c:RegisterEffect(ingredient_debuff_e)
+
+end
+
+---comment
+---@param e Effect
+---@param tc Card
+---@return integer
+function s.value(e, tc)
+	return CS.GetGrade(e:GetHandler()) * 100
 end
