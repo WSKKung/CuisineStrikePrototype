345fab618e57d373b9ca4a5787a41c5c7393b435
WSKKung <armkung493@gmail.com>
2023-08-14 22:24:39 +0700

    Allow targeting opponent dish

--- script/cuisine_strike_test/c197493107.lua
+++ script/cuisine_strike_test/c197493107.lua
@@ -10,10 +10,10 @@ Duel.LoadScript("cuisine_strike_common.lua")
 s.action_effect_options = {
 	type = "active",
 	target = function (e, tp, eg, ep, ev, re, r, rp, chk)
-		if chk==0 then return Duel.IsExistingMatchingCard(s.target_filter, tp, LOCATION_MZONE, 0, 1, nil) and CS.IsPlayerAbleToHeal(tp) end
+		if chk==0 then return Duel.IsExistingMatchingCard(s.target_filter, tp, LOCATION_MZONE, LOCATION_MZONE, 1, nil) and CS.IsPlayerAbleToHeal(tp) end
 	end,
 	operation = function (e, tp, eg, ep, ev, re, r, rp)
-		local g = Duel.SelectMatchingCard(tp, s.target_filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
+		local g = Duel.SelectMatchingCard(tp, s.target_filter, tp, LOCATION_MZONE, LOCATION_MZONE, 1, 1, nil)
 		local tc = g:GetFirst()
 		if tc then
 			CS.HealPlayer(tp, tc:GetAttack())
