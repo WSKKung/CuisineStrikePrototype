12e1a4a4175c4287f8aff2d15dbdf049a1a3f627
WSKKung <armkung493@gmail.com>
2023-08-14 22:24:55 +0700

    Rework new effect

--- script/cuisine_strike_test/c197493017.lua
+++ script/cuisine_strike_test/c197493017.lua
@@ -6,6 +6,9 @@ local s, id = GetID()
 
 Duel.LoadScript("cuisine_strike_common.lua")
 
+s.hp_cost = 300
+s.mill_cost_count = 3
+
 function s.initial_effect(c)
 
 	CS.InitCommonEffects(c)
@@ -14,11 +17,13 @@ function s.initial_effect(c)
 	-- cook summon procedures
 	Fusion.AddProcMixRep(c, true, true, aux.FilterBoolFunctionEx(Card.IsRace, CS.CLASS_GRAIN), 1, 2)
 
-	-- heal
+	-- draw
 	local e1 = Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id, 0))
 	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	e1:SetCountLimit(1)
@@ -28,36 +33,29 @@ end
 
 ---@type CardFilterFunction
 function s.filter(c)
-	return CS.IsDishCard(c) and (c:GetAttack() > 0 or c:GetDefense() > 0)
+	return CS.IsDishCard(c)
+end
+
+---@type ConditionFunction
+function s.condition(e, tp, eg, ep, ev, re, r, rp)
+	return Duel.IsExistingMatchingCard(s.filter, tp, 0, LOCATION_MZONE, 1, nil)
+end
+
+
+---@type CostFunction
+function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk==0 then return Duel.GetLP(tp) > s.hp_cost and Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) >= s.mill_cost_count + 1 end
+	Duel.PayLPCost(tp, s.hp_cost)
+	Duel.DiscardDeck(tp, s.mill_cost_count, REASON_COST)
 end
 
 ---@type TargetFunction
 function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
 	local c = e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_MZONE, LOCATION_MZONE, 1, c) and CS.IsPlayerAbleToHeal(tp) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp, 1) end
 end
 
 ---@type OperationFunction
 function s.operation(e, tp, eg, ep, ev, re, r, rp)
-	local c = e:GetHandler()
-	local tc = Duel.SelectMatchingCard(tp, s.filter, tp, LOCATION_MZONE, LOCATION_MZONE, 1, 1, c):GetFirst()
-	if not tc then return end
-
-	local heal_value = 0
-	if tc:GetAttack() == 0 then heal_value = tc:GetDefense()
-	elseif tc:GetDefense() == 0 then heal_value = tc:GetAttack()
-	else
-		local choice = Duel.SelectOption(tp, true, aux.Stringid(id, 1), aux.Stringid(id, 2))
-		-- select atk
-		if choice == 0 then
-			heal_value = tc:GetAttack()
-		-- select def
-		else
-			heal_value = tc:GetDefense()
-		end
-	end
-
-	if heal_value > 0 then
-		CS.HealPlayer(tp, heal_value)
-	end
+	Duel.Draw(tp, 1, REASON_EFFECT)
 end
